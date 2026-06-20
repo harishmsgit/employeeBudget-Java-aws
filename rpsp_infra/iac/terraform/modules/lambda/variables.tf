949fb3c8ef8f2c0687b5c14a67e60aff018e1676
harishmsgit <senharishms108@gmail.com>
2026-06-20 14:12:00 +0530

    fix(terraform): valid multiline variable blocks in batch and lambda modules

--- rpsp_infra/iac/terraform/modules/lambda/variables.tf
+++ rpsp_infra/iac/terraform/modules/lambda/variables.tf
@@ -2,6 +2,15 @@ variable "environment"        { type = string }
 variable "name_prefix"        { type = string }
 variable "vpc_id"             { type = string }
 variable "subnet_ids"         { type = list(string) }
-variable "security_group_ids" { type = list(string); default = [] }
-variable "sns_topic_arn"      { type = string; default = "" }
-variable "lambda_source_dir"  { type = string; description = "Path to Python source directory" }
+variable "security_group_ids" {
+	type    = list(string)
+	default = []
+}
+variable "sns_topic_arn" {
+	type    = string
+	default = ""
+}
+variable "lambda_source_dir" {
+	type        = string
+	description = "Path to Python source directory"
+}
